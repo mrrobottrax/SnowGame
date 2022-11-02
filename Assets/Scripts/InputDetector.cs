96bf09eb5b681823a21247e81543fab1f9bb0c11
mrrobottrax <59744880+mrrobottrax@users.noreply.github.com>
2022-11-02 10:51:27 -0400

    Update InputDetector

--- Assets/Scripts/InputDetector.cs
+++ Assets/Scripts/InputDetector.cs
@@ -9,6 +9,14 @@ public class InputDetector : MonoBehaviour
     bool mid;
     bool right;
 
+    [SerializeField] GameObject leftGo;
+    [SerializeField] GameObject midGo;
+    [SerializeField] GameObject rightGo;
+
+    float leftTimer = 0;
+    float midTimer = 0;
+    float rightTimer = 0;
+
     void Start()
     {
         
@@ -16,20 +24,53 @@ public class InputDetector : MonoBehaviour
 
     void Update()
     {
+        if (leftTimer <= 0)
+        {
+            leftGo.SetActive(false);
+        }
+        else
+        {
+            leftTimer -= Time.deltaTime;
+            leftGo.SetActive(true);
+        }
+
+        if (midTimer <= 0)
+        {
+            midGo.SetActive(false);
+        }
+        else
+        {
+            midTimer -= Time.deltaTime;
+            midGo.SetActive(true);
+        }
+
+        if (rightTimer <= 0)
+        {
+            rightGo.SetActive(false);
+        }
+        else
+        {
+            rightTimer -= Time.deltaTime;
+            rightGo.SetActive(true);
+        }
+
         // Detect the first frame a button is pressed
         if (Input.GetKey(KeyCode.A) && !left)
         {
             left = true;
+            leftTimer = 1;
             Debug.Log("Left");
         }
         if (Input.GetKey(KeyCode.W) && !mid)
         {
             mid = true;
+            midTimer = 1;
             Debug.Log("Mid");
         }
         if (Input.GetKey(KeyCode.D) && !right)
         {
             right = true;
+            rightTimer = 1;
             Debug.Log("Right");
         }
 
